26dcf4e5be74c898060d9eb0fb0f35b9da1e2502
VIBHOR AGARWAL <vibhor20349@iiitd.ac.in>
2022-04-20 01:18:28 +0530

    tried register page

--- market/routes.py
+++ market/routes.py
@@ -1,7 +1,26 @@
-from flask import render_template
+from flask import render_template, request
+from market import my_sql
 from market import app
 
 
-@app.route('/register')
+@app.route('/home')
+@app.route('/')
 def home():
-    return render_template('register.html')
+    return render_template('home.html')
+
+
+
+@app.route('/customerRegister',methods=['GET','POST'])
+def customerRegister():
+    if request.method=='POST':
+        custDetails = request.form
+        First_Name = custDetails['First_Name']
+        Last_Name = custDetails['Last_Name']
+        Email = custDetails['Email']
+        Mobile_No = custDetails['Mobile_No']
+        Password = custDetails['Password']
+        cur = my_sql.connection.cursor()
+        cur.execute("INSERT INTO customer(First_Name,Last_Name,Email,Mobile_No,Password) VALUES(%s %s)",First_Name,Last_Name,Email,Mobile_No,Password)
+        my_sql.connection.commit()
+        cur.close()
+    return render_template('customerRegister.html')
