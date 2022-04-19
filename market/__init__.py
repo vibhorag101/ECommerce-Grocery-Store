bfbc8118dcd33c463aa2eafac7e0f57a5671b54a
VIBHOR AGARWAL <vibhor20349@iiitd.ac.in>
2022-04-19 23:51:09 +0530

    initial commit

--- market/__init__.py
+++ market/__init__.py
@@ -4,10 +4,10 @@ from flask_mysqldb import MySQL
 
 
 app = Flask(__name__)
-from market import routes
+# from market import routes
 
 # Configure database
-db = yaml.load(open('db.yaml'));
+db = yaml.load (open('database.yaml'))
 app.config['MYSQL_HOST'] = db['mysql_host']
 app.config['MYSQL_USER'] = db['mysql_user']
 app.config['MYSQL_PASSWORD'] = db['mysql_password']
--- run.py
+++ run.py
@@ -1,2 +1,4 @@
+# import os
 from market import app
+# BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 app.run(debug=True)
