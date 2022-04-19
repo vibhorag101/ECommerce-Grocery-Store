4bc63ad2e85c1010d0ea0abc8e52f94edd7b6f6f
Anshak Goel <anshak20283@iiitd.ac.in>
2022-04-20 03:19:46 +0530

    Has Seller

--- market/routes.py
+++ market/routes.py
@@ -2,6 +2,7 @@ from flask import render_template, request
 from pyparsing import nums
 from market import my_sql
 from market import app
+import random
 
 
 @app.route('/home')
@@ -37,3 +38,22 @@ def adminRegister():
         my_sql.connection.commit()
         cur.close()
     return render_template('adminRegister.html')
+
+@app.route('/sellerRegister',methods=['GET','POST'])
+def sellerRegister():
+    if request.method=='POST':
+        sellerDetails = request.form
+        First_Name = sellerDetails['First_Name']
+        Last_Name = sellerDetails['Last_Name']
+        Email = sellerDetails['Email']
+        Password = sellerDetails['Password']
+        Mobile_No = sellerDetails['Phone_Number']
+        POO = sellerDetails['Place_Of_Operation']
+        cur = my_sql.connection.cursor()
+        rand_admin = cur.execute("SELECT Admin_ID FROM admin")
+        print("Printing data type here" + str(type(rand_admin)))
+        F_key = random.choice(rand_admin)
+        cur.execute("INSERT INTO seller(First_Name,Last_Name,Email,Phone_Number,Password,Place_Of_Operation,Admin_ID) VALUES(%s, %s, %s,%s,%s,%s, %d)",(First_Name,Last_Name,Email,Mobile_No,Password,POO, F_key))
+        my_sql.connection.commit()
+        cur.close()
+    return render_template('sellerRegister.html')
