d6139f21a24e521db28f0be9497e6fd3a9dc1861
VIBHOR AGARWAL <vibhor20349@iiitd.ac.in>
2022-04-25 03:55:17 +0530

    Corrected Dump

--- Dump.sql
+++ Dump.sql
@@ -23,12 +23,12 @@ DROP TABLE IF EXISTS `admin`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `admin` (
-  `Admin_ID` int NOT NULL,
+  `Admin_ID` int NOT NULL AUTO_INCREMENT,
   `First_Name` varchar(15) NOT NULL,
   `Last_Name` varchar(15) DEFAULT NULL,
   `Admin_Password` varchar(20) NOT NULL,
   PRIMARY KEY (`Admin_ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -472,4 +472,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-04-25  1:52:45
+-- Dump completed on 2022-04-25  3:54:25
